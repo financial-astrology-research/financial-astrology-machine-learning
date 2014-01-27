71563c72276791ec52bc5b1d4cde2eb2cafdbd4f
Pablo Cerda <pablo@citla.com>
2014-01-27 08:33:29 -0600

    Modified GA options: pcrossover to 0.4 and pmutation to 0.3

--- analysis.r
+++ analysis.r
@@ -2556,7 +2556,7 @@ cmpTestPlanetsSignificanceRelative <- function(execfunc, sinkfile, ...) {
     planetslist <- multipleOpenPlanets(planetsfile, degsplits)
 
     ga("real-valued", fitness=relativeTrendFitness, names=varnames, parallel=TRUE,
-       monitor=gaMonitor, maxiter=200, run=50, popSize=500, min=minvals, max=maxvals, pcrossover = 0.5, pmutation = 0.2,
+       monitor=gaMonitor, maxiter=200, run=50, popSize=500, min=minvals, max=maxvals, pcrossover = 0.4, pmutation = 0.3,
        selection=gaint_rwSelection, mutation=gaint_raMutation, crossover=gaint_spCrossover, population=gaint_Population,
        planetslist=planetslist, securityfile=securityfile, planetsfile=planetsfile, tsdate=tsdate, tedate=tedate, vsdate=vsdate,
        vedate=vedate, csdate=csdate, cedate=cedate, fittype=fittype, dateformat=dateformat)
