b118e846358521e6e30ba7be2b5759c5765cbd96
Pablo Cerda <pablo@citla.com>
2014-02-21 08:29:35 -0600

    Removed a160 and replaced a150 by a154. Changed to orbs 2_6_12.

--- analysis.r
+++ analysis.r
@@ -79,10 +79,10 @@ aspectTypesCols <- c('SUT', 'MOT', 'MET', 'VET', 'MAT', 'JUT', 'SAT', 'URT', 'NE
 planetsBaseCols <- c('SU', 'MO', 'ME', 'VE', 'MA', 'CE', 'JU', 'SA', 'UR', 'NE', 'PL', 'NN')
 
 # Aspects and orbs
-aspects            <- c(0 , 30, 36, 40, 45, 51, 60, 72, 80, 90, 103, 108, 120, 135, 144, 150, 160, 180)
-deforbs            <- c(12, 2 , 2 , 2 , 2 , 2 , 7 , 2 , 2 , 7 , 2  , 2  , 7  , 2  , 2  , 2  , 2  , 12)
-defaspectsenergy   <- c(10, 2 , 2 , 2 , 2 , 2 , 6 , 2 , 2 , 6 , 2  , 2  , 6  , 2  , 2  , 2  , 2  , 10)
-defaspectspolarity <- c(1 , 0 , 0 , 1 , 0 , 1 , 0 , 1 , 0 , 0 , 0  , 0  , 1  , 0  , 1  , 0  , 1  , 0)
+aspects            <- c(0 , 30, 36, 40, 45, 51, 60, 72, 80, 90, 103, 108, 120, 135, 144, 154, 180)
+deforbs            <- c(12, 2 , 2 , 2 , 2 , 2 , 6 , 2 , 2 , 6 , 2  , 2  , 6  , 2  , 2  , 2  , 12)
+defaspectsenergy   <- c(10, 2 , 2 , 2 , 2 , 2 , 6 , 2 , 2 , 6 , 2  , 2  , 6  , 2  , 2  , 2  , 10)
+defaspectspolarity <- c(1 , 0 , 0 , 1 , 0 , 1 , 0 , 1 , 0 , 0 , 0  , 0  , 1  , 0  , 1  , 0  , 0)
 
 aspOrbsCols <- as.character(apply(expand.grid(aspects, planetsBaseCols[1:(length(planetsBaseCols)-1)]), 1, function(x) paste(x[2], x[1], sep='')))
 zodDegrees <- seq(0, 360, by=2)
