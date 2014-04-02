a8697265e12fd8aba5c6fcc4e0b959857b13b37e
Pablo Cerda <pablo@citla.com>
2014-04-02 00:29:55 -0600

    Moved the get YAHOO symbols data to analysis.r file.

--- analysis.r
+++ analysis.r
@@ -13,6 +13,8 @@ options(width=130)
 options(error=recover)
 enableJIT(0)
 enableWD <- FALSE
+startDate = as.Date("1970-01-01")
+maxretry <- 1
 
 `%ni%` <- Negate(`%in%`)
 planetsBaseCols <- c('SU', 'MO', 'ME', 'VE', 'MA', 'CE', 'JU', 'SA', 'UR', 'NE', 'PL', 'NN')
@@ -997,4 +999,28 @@ securityPeaksValleys <- function(security, span=50, plotfile="peaks_valleys") {
   }
 
   dev.off()
+}
+
+processGetSymbol <- function(symbol) {
+  for(t in 1:maxretry) {
+    tryCatch({
+      cat("Downloading ", symbol, "\t\t Attempt: ", t , "/", maxretry, "\n")
+      symbol.df <- getSymbols(symbol, src="yahoo", from=startDate, env=NULL, return.class='data.frame')
+      filename <- paste('./stocks/', symbol, ".csv", sep='')
+      symbol.df <- cbind(rownames(symbol.df), symbol.df)
+      names(symbol.df) <- c('Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close')
+      write.csv(symbol.df, file=filename, row.names=F)
+      cat("Sucessfully saved the stock data to ", filename, "\n")
+      return(1)
+    }, error = function(e) {
+      print(e)
+      return(0)
+    })
+  }
+}
+
+getMySymbolsData  <- function(listfile) {
+  #Load the list of ticker symbols from a csv, each row contains a ticker
+  symbolsls <- read.csv(paste("./symbols/", listfile, '.csv', sep=''),  header=F, stringsAsFactors=F)
+  res <- lapply(symbolsls$V1, processGetSymbol)
 }
