cf39c789d091e3859ed85c472d5fdfd5fc844216
Pablo Cerda <pablo@citla.com>
2014-04-23 22:32:14 -0500

    Increased the popsize to 1500, elitism to 100 and maxiter to 100.

--- analysis.r
+++ analysis.r
@@ -750,8 +750,8 @@ cmpTestPlanetsSignificanceRelative <- function(execfunc, sinkfile, ...) {
       sink(sinkpathfile, append=T)
     }
 
-    ga("real-valued", fitness=relativeTrendFitness, parallel=TRUE, monitor=gaMonitor, maxiter=50, run=50, min=minvals, max=maxvals,
-       popSize=600, elitism = 30, pcrossover = 0.9, pmutation = 0.1,
+    ga("real-valued", fitness=relativeTrendFitness, parallel=TRUE, monitor=gaMonitor, maxiter=100, run=50, min=minvals, max=maxvals,
+       popSize=1500, elitism = 100, pcrossover = 0.9, pmutation = 0.1,
        selection=gaint_rwSelection, mutation=gaint_raMutation, crossover=gaint_spCrossover, population=gaint_Population,
        planetsorig=planetsorig, securityfile=securityfile, planetsfile=planetsfile, tsdate=tsdate, tedate=tedate, vsdate=vsdate,
        vedate=vedate, csdate=csdate, cedate=cedate, fittype=fittype, mapricefs=mapricefs, mapricesl=mapricesl, dateformat=dateformat)
