2169a100a63d235ec98894072a858c4e8ae7ad29
Pablo Cerda <pablo@citla.com>
2014-05-27 22:56:06 -0500

    Removed the aspects merge that is duplicated with indicators value. Added lagged orb difference calculation so we can determine if is a applicative or separative aspect.

--- analysis.r
+++ analysis.r
@@ -1480,15 +1480,11 @@ dailySignificantAspectsIndicators <- function(planets, security, sdate, edate, d
   indicators <- cols[grep('ASP.', cols)]
   # Melt the distances
   planets.long <- melt(planets.aspsday, id.var=c('Date'), measure.var=indicators)
+  planets.long[, type := 'spaspect']
   planets.long[, variable := paste(substr(variable, 1, 2), substr(variable, 6, 9), sep='')]
   planets.long <- planets.long[!is.na(value),]
   planets.long <- planets.long[, value := as.factor(value)]
   setkeyv(planets.long, c('Date', 'variable'))
-  # Melt the aspects
-  planets.long.asps <- melt(planets.aspsday, id.var=c('Date'), measure.var=cols[grep('ASP.', cols)])
-  planets.long.asps[, variable := paste(substr(variable, 1, 2), substr(variable, 6, 9), sep='')]
-  setnames(planets.long.asps, c('Date', 'variable', 'asp'))
-  setkeyv(planets.long.asps, c('Date', 'variable'))
   # Melt the orbs
   planets.long.orbs <- melt(planets.aspsday, id.var=c('Date'), measure.var=cols[grep('ORB.', cols)])
   planets.long.orbs[, variable := paste(substr(variable, 1, 2), substr(variable, 6, 9), sep='')]
@@ -1500,8 +1496,8 @@ dailySignificantAspectsIndicators <- function(planets, security, sdate, edate, d
   freq[, variable := paste(substr(variable, 1, 2), substr(variable, 6, 9), sep='')]
   siglon.aspects.daily.freq <- merge(planets.long, freq, by=c('variable', 'value'))
   # Add the orbs & aspects
-  siglon.aspects.daily.freq <- merge(siglon.aspects.daily.freq, planets.long.asps, by=c('Date', 'variable'))
   siglon.aspects.daily.freq <- merge(siglon.aspects.daily.freq, planets.long.orbs, by=c('Date', 'variable'))
+  siglon.aspects.daily.freq[, orbdif := round(orb - Lag(orb), digits=2), by=c('variable', 'value')]
 
   setkey(siglon.aspects.daily.freq, Date)
   return(siglon.aspects.daily.freq)
