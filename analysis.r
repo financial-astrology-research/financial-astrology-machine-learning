251801bf2269c84b222ba922fca8fa4838d2053e
Pablo Cerda <pablo@citla.com>
2014-08-09 16:18:40 -0500

    Reduced GA maxiter to 50.

--- analysis.r
+++ analysis.r
@@ -813,7 +813,7 @@ cmpTestPlanetsSignificanceRelative <- function(execfunc, ...) {
       securityfile <- paste(sectype, symbol, sep="/")
       predfile <- paste('b', benchno, '/', symbol, '_', benchno, sep="")
 
-      ga("real-valued", fitness=relativeTrendFitness, parallel=TRUE, monitor=gaMonitor, maxiter=60, run=50, min=minvals, max=maxvals,
+      ga("real-valued", fitness=relativeTrendFitness, parallel=TRUE, monitor=gaMonitor, maxiter=50, run=50, min=minvals, max=maxvals,
          popSize=1000, elitism = 100, pcrossover = 0.9, pmutation = 0.1,
          selection=gaint_rwSelection, mutation=gaint_raMutation, crossover=gaint_spCrossover, population=gaint_Population,
          topn=topn, securityfile=securityfile, planetsfile=planetsfile, predfile=predfile,
