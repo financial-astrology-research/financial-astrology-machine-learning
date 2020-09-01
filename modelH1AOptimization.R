3f329875befce383d1f57bba5e4e75caba35d962
Pablo Cerda <pablo@citla.com>
2020-09-01 11:46:49 +0200

    Increase the parameters search space by 1, add new best suggestion

--- modelH1AOptimization.R
+++ modelH1AOptimization.R
@@ -1,6 +1,8 @@
 library(GA)
+library(parallel)
+library(doParallel)
 source("./indicatorPlots.r")
-setDTthreads(0)
+setDTthreads(6)
 
 symbol <- "EOS-USD"
 securityData <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2010-01-01")
@@ -10,45 +12,47 @@ securityData[, fold := kfold(securityData, k = 5)]
 hourlyAspects <- prepareHourlyAspectsModelH1()
 enOpts <- seq(-1, 1, by = 1)
 #searchResult <- random_search(
-#  predictSecurityModelH1A,
-#  params = list(
-#    en0 = enOpts, en30 = enOpts, en45 = enOpts, en60 = enOpts, en90 = enOpts,
-#    en120 = enOpts, en135 = enOpts, en150 = enOpts, en180 = enOpts
-#  ),
-#  speedDecay = 0.001,
-#  security = securityData,
-#  hourlyAspects = hourlyAspects,
-#  output = "data.frame",
-#  n.sample = 10000,
+# predictSecurityModelH1A,
+# params = list(
+# en0 = enOpts, en30 = enOpts, en45 = enOpts, en60 = enOpts, en90 = enOpts,
+# en120 = enOpts, en135 = enOpts, en150 = enOpts, en180 = enOpts
+# ),
+# speedDecay = 0.001,
+# security = securityData,
+# hourlyAspects = hourlyAspects,
+# output = "data.frame",
+# n.sample = 10000,
 #)
 #
 #print(searchResult)
 
 # Best parameters results:
-# en0.test en30.test en45.test en60.test en90.test en120.test en135.test en150.test en180.test    output
-#     -1         0         0         0         1          0          0          1          0     0.3969207
-#     -1        -1         0         0         1          0         -1          1          0     0.4151665
-#     -1         0         0         0         1          1         -1          1          0     0.3942058
-#     -1         0         0         0         1          1         -1          1          0     0.3942058
+# en0.test en30.test en45.test en60.test en90.test en120.test en135.test en150.test en180.test output
+# -1 0 0 0 1 0 0 1 0 0.3969207
+# -1 -1 0 0 1 0 -1 1 0 0.4151665
+# -1 0 0 0 1 1 -1 1 0 0.3942058
+# -1 0 0 0 1 1 -1 1 0 0.3942058
 
 # Suggest the best solutions found through random search.
-solutions < rbind(
+solutions <- rbind(
   c(-3, -1, 1, -1, 1, 2, -1, 2, -1),
   c(-1, 0, 0, 0, 1, 0, 0, 1, 0),
   c(-1, -1, 0, 0, 1, 0, -1, 1, 0),
   c(-1, 0, 0, 0, 1, 1, -1, 1, 0),
   c(-1, 0, 0, 0, 1, 1, -1, 1, 0),
+  c(-2, -2, 0, -1, 2, -1, -1, 2, 0),
+  c(-3, -2, 0, -1, 2, 0, -1, 2, 0)
 )
 
 gar <- ga(
   "real-valued",
   fitness = predictSecurityModelH1A,
-  lower = c(-3, -2, -2, -2, -3, -2, -2, -2, -3),
-  upper = c(3, 2, 2, 2, 3, 2, 2, 2, 3),
+  lower = c(-3, -3, -3, -3, -3, -3, -3, -3, -3),
+  upper = c(3, 3, 3, 3, 3, 3, 3, 3, 3),
   names = c('a0', 'a30', 'a45', 'a60', 'a90', 'a120', 'a135', 'a150', 'a180'),
   suggestions = solutions,
   popSize = 200, elitism = 20, pcrossover = 0.9, pmutation = 0.1,
-  maxiter = 100, run = 20,
+  maxiter = 100, run = 30,
   selection = gaint_rwSelection, mutation = gaint_raMutation,
   crossover = gaint_spCrossover, population = gaint_Population,
   parallel = F, monitor = gaMonitor, keepBest = T,
@@ -57,3 +61,5 @@ gar <- ga(
 
 summary(gar)
 plot(gar)
+
+#,PARAMS,-,en0:,-3,en30:,-2,en45:,0,en60:,-1,en90:,2,en120:,0,en135:,-1,en150:,2,en180:,0
