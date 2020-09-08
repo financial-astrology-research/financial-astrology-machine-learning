70b469965e4492fefe6562b4bc973f7e3133f1ab
Pablo Cerda <pablo@citla.com>
2020-09-08 12:48:34 +0200

    ModelK: daily partil aspects with category (up/down) RandomForest

---
+++ researchModelK.R
@@ -0,0 +1,80 @@
+# Title     : Research for model K using Random Forest trees.
+# Objective : Evaluate how well fits the RF model to multiples symbols.
+# Created by: pablocc
+# Created on: 08/09/2020
+
+library(caret)
+library(magrittr)
+library(parallel)
+library(psych)
+library(randomForest)
+library(rattle)
+library(tidyverse)
+source("./indicatorPlots.r")
+
+symbol <- "EOS-USD"
+securityData <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2010-01-01")
+dailyAspects <- prepareHourlyAspectsModelK()
+dailyAspectsPrice <- merge(securityData[, c('Date', 'diffPercent')], dailyAspects, by = "Date")
+dailyAspectsPrice[, result := cut(diffPercent, c(-1, 0, 1), c("down", "up"))]
+
+# Experiment with Random Forest model.
+aspectViewRaw <- dailyAspectsPrice[p.x == "MO"]
+aspectsT <- paste("a", aspects, sep = "")
+aspectsX <- paste("a", aspects, ".x", sep = "")
+aspectsY <- paste("a", aspects, ".y", sep = "")
+#selectCols <- c("result", "acx", aspectsX, "spp", "dcp", "zx", "zy", "MO", "ME", "VE", "SU", "MA", "JU", "SA")
+#selectCols <- c("result", aspectsX, "ME.x", "VE.x", "MA.x", "JU.x", "SA.x", "NN.x")
+selectCols <- c("result", aspectsX)
+aspectView <- aspectViewRaw[, ..selectCols]
+trainIndex <- createDataPartition(aspectView$result, p=0.70, list=FALSE)
+aspectViewTrain <- aspectView[trainIndex,]
+aspectViewTest <- aspectView[-trainIndex,]
+
+control <- trainControl(
+  method = "repeatedcv",
+  number = 10,
+  repeats = 2,
+  search = "random",
+  allowParallel = T
+)
+
+tree1 = train(
+  formula(result ~ .),
+  data = aspectViewTrain,
+  method = "rf",
+  metric = "Accuracy",
+  tuneLength = 2,
+  ntree = 100,
+  trControl = control,
+  importance = T
+)
+#summary(tree1)
+
+effect_p <- tree1 %>% predict(newdata = aspectViewTrain)
+# Prediction results on train.
+table(
+  actualclass = aspectViewTrain$result,
+  predictedclass = effect_p
+) %>%
+  confusionMatrix() %>%
+  print()
+
+effect_p <- tree1 %>% predict(newdata = aspectViewTest)
+# Prediction results on test.
+table(
+  actualclass = aspectViewTest$result,
+  predictedclass = effect_p
+) %>%
+  confusionMatrix() %>%
+  print()
+
+#saveRDS(tree1, "./models/MO_general_rf4.rds")
+
+selectCols2 <- selectCols[selectCols != "result"]
+futureAspects <- dailyAspects[Date >= as.Date("2020-08-20") & p.x == "MO",]
+futureAspectsFeatures <- futureAspects[, ..selectCols2]
+effect_p <- tree1 %>% predict(newdata = futureAspectsFeatures)
+futureAspects$effect_p <- effect_p
+marketPrediction <- futureAspects[, c('Date', "origin", "aspect", "effect_p")]
+view(marketPrediction)
