22fff1741cc3f48c4fd1c380af9fc281436f4eb8
Pablo Cerda <pablo@citla.com>
2020-09-24 18:44:47 +0200

    Rename variables to avoid environment values override

--- accuracyModelK.R
+++ accuracyModelK.R
@@ -2,16 +2,18 @@
 
 # Test latest price period accuracy.
 source("analysis.r")
-symbol <- "BAT-USD"
-securityData <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2020-08-16")
+symbolTest <- "LINK-USD"
+securityDataTest <- mainOpenSecurity(symbolTest, 14, 28, "%Y-%m-%d", "2020-08-16")
+# basePath <- "~/Sites/own/trading-signal-processing/csv_indicators/"
+basePath <- "~/Desktop/"
 
-symbolNormalized <- str_replace(symbol, "-", "")
+symbolNormalized <- str_replace(symbolTest, "-", "")
 indicatorFile <- paste("ml-", str_replace(symbolNormalized, "USD", "USDT"), "-daily", sep = "")
 dailyIndicator <- fread(
-  paste("/Users/pablocc/Sites/own/trading-signal-processing/csv_indicators/", indicatorFile, ".csv", sep = "")
+  paste(basePath, indicatorFile, ".csv", sep = "")
 )
 dailyIndicator[, Date := as.Date(Date)]
-dailyIndicator <- merge(securityData[, c('Date', 'diffPercent')], dailyIndicator, by = "Date")
+dailyIndicator <- merge(securityDataTest[, c('Date', 'diffPercent')], dailyIndicator, by = "Date")
 dailyIndicator[, Actual := as.character(cut(diffPercent, c(-100, 0, 100), c("sell", "buy")))]
 
 table(
