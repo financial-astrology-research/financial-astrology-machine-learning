a0f64d8a9154d4cf32d1e74a233313a2c6332c17
Pablo Cerda <pablo@citla.com>
2020-09-25 21:23:39 +0200

    Increase model test period and check MSE metric

--- fitModelL.R
+++ fitModelL.R
@@ -10,13 +10,14 @@ library(rattle)
 library(gvlma)
 library(arm)
 library(glmulti)
+library(modelr)
 source("./indicatorPlots.r")
 
 dailyAspects <- prepareHourlyAspectsModelK()
 symbol <- "LINK-USD"
 securityData <- mainOpenSecurity(
   symbol, 14, 28, "%Y-%m-%d",
-  "2010-01-01", "2020-08-31"
+  "2010-01-01", "2020-07-31"
 )
 
 aspectsX <- paste("a", aspects, ".x", sep = "")
@@ -28,7 +29,7 @@ selectCols <- c(
   #"zx", "zy", "aspect", "spd", "spp", "acx", "acy", "agt",
 )
 
-# Evaluate polarity effect on SU90 aspect.
+# Fit a90 aspects model.
 aspectViewRaw <- dailyAspects[p.x != "MO" & aspect == 90]
 aspectView <- aspectViewRaw[, ..selectCols]
 aspectView <- merge(securityData[, c('Date', 'diffPercent')], aspectView, by = "Date")
@@ -48,7 +49,7 @@ modelSearch <- glmulti(
   exclude=c("sp.y", "sp.x", "dc.x", "dc.y"),
   level = 2, marginality = F, intercept = F, crit = "aicc",
   method = "g", plotty = F,
-  popsize = 500, mutrate = 0.01, sexrate = 0.1, imm = 0.1,
+  popsize = 200, mutrate = 0.01, sexrate = 0.1, imm = 0.1,
 )
 
 # plot(modelSearch, type = "s")
@@ -67,10 +68,15 @@ modelFit %>% coefplot()
 # Validate with test subset.
 aspectViewTest$diffPredict <- predict(modelFit, aspectViewTest)
 aspectViewTest[, c('Date', 'diffPercent', 'diffPredict')]
+cor(aspectViewTest$diffPercent, aspectViewTest$diffPredict) %>% print()
+plot(aspectViewTest$diffPercent, aspectViewTest$diffPredict)
+with(aspectViewTest, mean((diffPercent - diffPredict) ^ 2)) %>% sqrt()
 
 # Validate with reserved data.
-securityDataTest <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2020-09-01")
+securityDataTest <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2020-08-01")
 aspectViewValidate <- aspectViewRaw[, ..selectCols]
 aspectViewValidate <- merge(securityDataTest[, c('Date', 'diffPercent')], aspectViewValidate, by = "Date")
 aspectViewValidate$diffPredict <- predict(modelFit, aspectViewValidate)
 aspectViewValidate[, c('Date', 'diffPercent', 'diffPredict')]
+cor(aspectViewValidate$diffPercent, aspectViewValidate$diffPredict) %>% print()
+with(aspectViewValidate, mean((diffPercent - diffPredict) ^ 2)) %>% sqrt()
