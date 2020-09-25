3abe2c9ad914d086532fa40bbd2a82e8849a1af3
Pablo Cerda <pablo@citla.com>
2020-09-25 20:36:03 +0200

    Fit, test and validate ModelL stages

--- fitModelL.R
+++ fitModelL.R
@@ -32,30 +32,26 @@ selectCols <- c(
 aspectViewRaw <- dailyAspects[p.x != "MO" & aspect == 90]
 aspectView <- aspectViewRaw[, ..selectCols]
 aspectView <- merge(securityData[, c('Date', 'diffPercent')], aspectView, by = "Date")
+trainIndex <- createDataPartition(aspectView$diffPercent, p = 0.80, list = FALSE)
+aspectViewTrain <- aspectView[trainIndex,]
+aspectViewTest <- aspectView[-trainIndex,]
 
 modelSearch <- glmulti(
   y = "diffPercent",
   xr = c(
-    "sp.y", "sp.x", "dc.x",
-    "a30.x", "a45.x", "a60.x", "a90.x", "a120.x", "a135.x", "a150.x",
-    "MA", "JU", "SA", "UR.y", "PL"
+    "sp.y", "sp.x", "dc.x", "dc.y",
+    "a45.x", "a60.x", "a90.x", "a120.x", "a135.x",
+    "ME", "MA", "JU", "SA", "UR.y", "PL" # R2 = 0.38 - SIGNIFICANT
     #"MO", "SU", "ME", "VE", "MA", "JU", "SA", "UR.y", "NE", "PL" # R2 = 0.38
   ),
-  data = aspectView,
+  data = aspectViewTrain,
   exclude=c("sp.y", "sp.x", "dc.x", "dc.y"),
-  level = 2, marginality = F,intercept = F, crit = "aicc",
-  # minK = 0.2, maxK = 0.5,
-  minsize = 5, maxsize = 20,
-  confsetsize = 100,
+  level = 2, marginality = F, intercept = F, crit = "aicc",
   method = "g", plotty = F,
-  popsize = 300, mutrate = 0.01, sexrate = 0.1, imm = 0.1,
+  popsize = 500, mutrate = 0.01, sexrate = 0.1, imm = 0.1,
 )
 
-plot(modelSearch, type = "s")
-#plot(modelSearch)
-top <- weightable(modelSearch)
-top <- top[top$aic <= min(top$aic) + 2,]
-
+# plot(modelSearch, type = "s")
 summary(modelSearch@objects[[1]])
 
 # Review the best fit.
@@ -67,3 +63,14 @@ modelFit <- lm(
 modelFit %>% summary()
 modelFit %>% plot()
 modelFit %>% coefplot()
+
+# Validate with test subset.
+aspectViewTest$diffPredict <- predict(modelFit, aspectViewTest)
+aspectViewTest[, c('Date', 'diffPercent', 'diffPredict')]
+
+# Validate with reserved data.
+securityDataTest <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2020-09-01")
+aspectViewValidate <- aspectViewRaw[, ..selectCols]
+aspectViewValidate <- merge(securityDataTest[, c('Date', 'diffPercent')], aspectViewValidate, by = "Date")
+aspectViewValidate$diffPredict <- predict(modelFit, aspectViewValidate)
+aspectViewValidate[, c('Date', 'diffPercent', 'diffPredict')]
