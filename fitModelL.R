bb8ade972a2f6daf1f4943643f1dfdc4ffae6ec0
Pablo Cerda <pablo@citla.com>
2020-09-26 14:10:38 +0200

    Experiment speed/declination/aspects derivative variables fit

--- fitModelL.R
+++ fitModelL.R
@@ -10,54 +10,87 @@ library(rattle)
 library(gvlma)
 library(arm)
 library(glmulti)
-library(modelr)
 source("./indicatorPlots.r")
 
 dailyAspects <- prepareHourlyAspectsModelK()
 symbol <- "LINK-USD"
 securityData <- mainOpenSecurity(
   symbol, 14, 28, "%Y-%m-%d",
-  "2010-01-01", "2020-07-31"
+  "2010-01-01", "2020-06-30"
 )
 
+aspectsT <- paste("a", aspects, sep = "")
 aspectsX <- paste("a", aspects, ".x", sep = "")
+aspectsY <- paste("a", aspects, ".y", sep = "")
+aspectsG <- paste("a", aspects, ".g", sep = "")
+aspectsAll <- c(aspectsT, aspectsX, aspectsY, aspectsG)
+planetsAll <- c(
+  "ME.x", "VE.x", "SU.x", "MA.x", "JU.x", "NN.x", "SA.x", "UR.x",
+  "MO", "SU", "ME", "VE", "MA", "JU", "SA", "NE", "PL",
+  "ME.y", "VE.y", "SU.y", "MA.y", "JU.y", "NN.y", "SA.y", "UR.y"
+)
+#dailyAspectsNorm <- dailyAspects[, c(planetsAll) := lapply(.SD, function(x) ifelse(x > 0, 1, 0)), .SDcols = planetsAll]
+
 selectCols <- c(
   "Date",
-  aspectsX,
-  "sp.y", "sp.x", "dc.y", "dc.x",
-  "MO", "SU", "ME", "VE", "MA", "JU", "SA", "NE", "UR.y", "PL"
-  #"zx", "zy", "aspect", "spd", "spp", "acx", "acy", "agt",
+  aspectsAll,
+  planetsAll,
+  "sp.x", "spi.x", "sp.y", "spi.y",
+  "dc.y", "dc.x",
+  "zx", "zy",
+  "spd", "spdi", "spp", "spr", "spri",
+  "dcd", "dcdi", "dcp", "dcr", "dcri",
+  "acx", "acy", "agt",
+  "orb", "wd"
 )
 
 # Fit a90 aspects model.
-aspectViewRaw <- dailyAspects[p.x != "MO" & aspect == 90]
+aspectViewRaw <- dailyAspects[p.x == "SU" & aspect == 90]
 aspectView <- aspectViewRaw[, ..selectCols]
 aspectView <- merge(securityData[, c('Date', 'diffPercent')], aspectView, by = "Date")
-trainIndex <- createDataPartition(aspectView$diffPercent, p = 0.80, list = FALSE)
-aspectViewTrain <- aspectView[trainIndex,]
-aspectViewTest <- aspectView[-trainIndex,]
+# trainIndex <- createDataPartition(aspectView$diffPercent, p = 0.80, list = FALSE)
 
 modelSearch <- glmulti(
   y = "diffPercent",
   xr = c(
-    "sp.y", "sp.x", "dc.x", "dc.y",
-    "a45.x", "a60.x", "a90.x", "a120.x", "a135.x",
-    "ME", "MA", "JU", "SA", "UR.y", "PL" # R2 = 0.38 - SIGNIFICANT
-    #"MO", "SU", "ME", "VE", "MA", "JU", "SA", "UR.y", "NE", "PL" # R2 = 0.38
+    #aspectsX,
+    #aspectsY,
+    aspectsT,
+    "orb",
+    "spd", "spdi", "spp", "spr", "spri",
+    "dcd", "dcdi", "dcp", "dcr", "dcri"
+    #"sp.x", "sp.y",
+    #"spi.x", "spi.y"
+    # "dc.y", "dc.x"
+    #"zx", "zy", "wd"
+    #"MO", "SU", "ME", "VE", "MA", "JU", "SA", "NE", "NN.x", "NN.y", "PL"
+    #"MO", "SU", "ME", "VE", "MA", "JU", "SA", "NE", "PL"
   ),
-  data = aspectViewTrain,
-  exclude=c("sp.y", "sp.x", "dc.x", "dc.y"),
-  level = 2, marginality = F, intercept = F, crit = "aicc",
+  data = aspectView,
+  #exclude=c("sp.y", "sp.x", "dc.x", "dc.y"),
+  #minsize = 15,
+  level = 1, marginality = F, intercept = T, crit = "bic",
   method = "g", plotty = F,
-  popsize = 200, mutrate = 0.01, sexrate = 0.1, imm = 0.1,
+  popsize = 500
+  #mutrate = 0.01, sexrate = 0.1, imm = 0.1,
 )
 
-# plot(modelSearch, type = "s")
+# aspectsT / minsize 10 = Adjusted R-squared:  0.41
+# aspectsG / minsize 10 = Adjusted R-squared:  0.25
+# aspectsX / minsize 10 = Adjusted R-squared:  0.34
+# aspectsY / minsize 10 = Adjusted R-squared:  0.32
+# aspectsY / aspectsY / minsize 10 = Adjusted R-squared: 0.49
+# 0.3501
+# 0.31
+# 0.42
+# 0.36
+
+plot(modelSearch, type = "s")
 summary(modelSearch@objects[[1]])
 
 # Review the best fit.
 modelFit <- lm(
-  modelSearch@objects[[1]]$formula,
+  modelSearch@objects[[2]]$formula,
   data = aspectView
 )
 
@@ -65,18 +98,11 @@ modelFit %>% summary()
 modelFit %>% plot()
 modelFit %>% coefplot()
 
-# Validate with test subset.
-aspectViewTest$diffPredict <- predict(modelFit, aspectViewTest)
-aspectViewTest[, c('Date', 'diffPercent', 'diffPredict')]
-cor(aspectViewTest$diffPercent, aspectViewTest$diffPredict) %>% print()
-plot(aspectViewTest$diffPercent, aspectViewTest$diffPredict)
-with(aspectViewTest, mean((diffPercent - diffPredict) ^ 2)) %>% sqrt()
-
 # Validate with reserved data.
-securityDataTest <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2020-08-01")
+securityDataTest <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2020-07-01")
 aspectViewValidate <- aspectViewRaw[, ..selectCols]
-aspectViewValidate <- merge(securityDataTest[, c('Date', 'diffPercent')], aspectViewValidate, by = "Date")
 aspectViewValidate$diffPredict <- predict(modelFit, aspectViewValidate)
+aspectViewValidate <- merge(securityDataTest[, c('Date', 'diffPercent')], aspectViewValidate, by = "Date")
 aspectViewValidate[, c('Date', 'diffPercent', 'diffPredict')]
 cor(aspectViewValidate$diffPercent, aspectViewValidate$diffPredict) %>% print()
-with(aspectViewValidate, mean((diffPercent - diffPredict) ^ 2)) %>% sqrt()
+with(aspectViewValidate, mean((diffPercent - diffPredict)^2)) %>% sqrt()
