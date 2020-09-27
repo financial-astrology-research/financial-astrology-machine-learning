b7b9017ce3e8aed580726c91d59088ffd6670954
Pablo Cerda <pablo@citla.com>
2020-09-27 19:05:51 +0200

    Research model that use X/Y cumulative energy differences

---
+++ fitModelLB.R
@@ -0,0 +1,100 @@
+# Title     : Generalized aspect per planet with cumulative energy diff instead of simple counts.
+# Created by: pablocc
+# Created on: 27/09/2020
+
+library(caret)
+library(magrittr)
+library(psych)
+library(plyr)
+library(rattle)
+library(gvlma)
+library(arm)
+library(glmulti)
+source("./analysis.r")
+source("./indicatorPlots.r")
+
+dailyAspects <- prepareHourlyAspectsModelLB()
+symbol <- "LINK-USD"
+securityData <- mainOpenSecurity(
+  symbol, 14, 28, "%Y-%m-%d",
+  "2010-01-01", "2020-06-30"
+)
+
+aspectType <- paste("ast", aspects, sep = "")
+aspectsT <- paste("a", aspects, sep = "")
+aspectsD <- paste("a", aspects, ".d", sep = "")
+aspectsX <- paste("a", aspects, ".x", sep = "")
+aspectsY <- paste("a", aspects, ".y", sep = "")
+aspectsAll <- c(aspectsT, aspectsD, aspectsX, aspectsY)
+#dailyAspectsNorm <- dailyAspects[, c(planetsAll) := lapply(.SD, function(x) ifelse(x > 0, 1, 0)), .SDcols = planetsAll]
+
+selectCols <- c(
+  "Date",
+  aspectType,
+  aspectsAll,
+  "sp.x", "spi.x", "sp.y", "spi.y",
+  "dc.y", "dc.x",
+  "retx", "rety",
+  "spd", "spdi", "spp", "spr", "spri",
+  "dcd", "dcdi", "dcp", "dcr", "dcri",
+  "orb", "apl", "sep"
+)
+
+# Fit a90 aspects model.
+aspectViewRaw <- dailyAspects[p.x %in% c('SU') & aspect %in% c(0, 60, 90, 120, 150, 180)]
+aspectView <- aspectViewRaw[, ..selectCols]
+aspectView <- merge(securityData[, c('Date', 'diffPercent')], aspectView, by = "Date")
+hist(aspectView$diffPercent)
+# trainIndex <- createDataPartition(aspectView$diffPercent, p = 0.80, list = FALSE)
+
+modelSearch <- glmulti(
+  y = "diffPercent",
+  xr = c(
+    aspectsD
+    #aspectsT
+    #aspectType,
+    #aspectsX,
+    #aspectsY
+    #"apl", "sep",
+    #"spd", "spdi",
+    #"dcd", "dcdi",
+    #"dcp", "dcr", "dcri",
+    #"dcr", "dcri",
+    #"retx", "rety"
+    #"sp.x", "sp.y",
+    #"spi.x", "spi.y"
+    #"dc.y", "dc.x"
+  ),
+  data = aspectView,
+  #exclude=c("sp.y", "sp.x", "dc.x", "dc.y"),
+  #minsize = 15,
+  level = 1, marginality = F, intercept = T, crit = "aicc",
+  method = "g", plotty = F,
+  popsize = 300
+  #mutrate = 0.01, sexrate = 0.1, imm = 0.1,
+)
+
+plot(modelSearch, type = "s")
+print(modelSearch@objects[[1]]$formula)
+
+# Review the best fit.
+modelFit <- lm(
+  modelSearch@objects[[1]]$formula,
+  data = aspectView
+)
+
+modelFit %>% summary()
+modelFit %>% plot()
+modelFit %>% coefplot()
+
+# Validate with reserved data.
+securityDataTest <- mainOpenSecurity(symbol, 14, 28, "%Y-%m-%d", "2020-07-01")
+aspectViewValidate <- aspectViewRaw[, ..selectCols]
+aspectViewValidate$diffPredict <- predict(modelFit, aspectViewValidate)
+aspectViewValidate <- merge(securityDataTest[, c('Date', 'diffPercent')], aspectViewValidate, by = "Date")
+aspectViewValidate[, c('Date', 'diffPercent', 'diffPredict')]
+plot(aspectViewValidate$diffPercent, aspectViewValidate$diffPredict)
+cor(aspectViewValidate$diffPercent, aspectViewValidate$diffPredict) %>% print()
+with(aspectViewValidate, mean((diffPercent - diffPredict)^2)) %>% sqrt()
+
+# CONCLUSION:
