47474a30b606f8c2a7ceb7ac362d1ad7356d591b
Pablo Cerda <pablo@citla.com>
2020-10-13 12:58:33 +0200

    Refactor to support different indicator files accuracy test

--- accuracyModelK.R
+++ accuracyModelK.R
@@ -1,25 +1,25 @@
 # Title     : Test accuracy of ModelK indicator predictions.
 
 # Test latest price period accuracy.
+library(caret)
 source("analysis.r")
 symbolTest <- "LINK-USD"
-securityDataTest <- mainOpenSecurity(symbolTest, 14, 28, "%Y-%m-%d", "2020-08-16")
-# basePath <- "~/Sites/own/trading-signal-processing/csv_indicators/"
-basePath <- "~/Desktop/"
+securityDataTest <- mainOpenSecurity(symbolTest, 14, 28, "%Y-%m-%d", "2020-09-13")
+#basePath <- "~/Sites/own/trading-signal-processing/csv_indicators/"
+basePath <- "~/Desktop/ModelsPred/"
 
 symbolNormalized <- str_replace(symbolTest, "-", "")
-indicatorFile <- paste("ml-", str_replace(symbolNormalized, "USD", "USDT"), "-daily", sep = "")
+indicatorFile <- "LINK-USD-predict-ensamble"
 dailyIndicator <- fread(
   paste(basePath, indicatorFile, ".csv", sep = "")
 )
 dailyIndicator[, Date := as.Date(Date)]
-dailyIndicator <- merge(securityDataTest[, c('Date', 'diffPercent')], dailyIndicator, by = "Date")
-dailyIndicator[, Actual := as.character(cut(diffPercent, c(-100, 0, 100), c("sell", "buy")))]
+dailyIndicator <- merge(securityDataTest[, c('Date', 'Eff', 'Actbin')], dailyIndicator, by = "Date")
 
+cat("DAILY EFFECT\n")
 table(
-  actualclass = as.character(dailyIndicator$Actual),
+  actualclass = as.character(dailyIndicator$Actbin),
   predictedclass = as.character(dailyIndicator$Action)
 ) %>%
   confusionMatrix() %>%
   print()
-
