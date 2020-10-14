8fd0bb481f47dd5faac29bd9e01e08d96dc2ab3d
Pablo Cerda <pablo@citla.com>
2020-10-14 16:24:21 +0200

    Indicator filenames shortcuts

--- accuracyModelK.R
+++ accuracyModelK.R
@@ -3,20 +3,25 @@
 # Test latest price period accuracy.
 library(caret)
 source("analysis.r")
-symbolTest <- "LINK-USD"
+symbolTest <- "BAT-USD"
 securityDataTest <- mainOpenSecurity(symbolTest, 14, 28, "%Y-%m-%d", "2020-09-13")
 #basePath <- "~/Sites/own/trading-signal-processing/csv_indicators/"
 basePath <- "~/Desktop/ModelsPred/"
 
 symbolNormalized <- str_replace(symbolTest, "-", "")
-indicatorFile <- "LINK-USD-predict-ensamble"
+#indicatorFile <- "ADA-USD-predict-ensamble"
+#indicatorFile <- "ZRX-USD-predict-ensamble"
+#indicatorFile <- "BAT-USD-predict-ensamble"
+#indicatorFile <- "ml-LINK-USD-daily-avnnet"
+#indicatorFile <- "ml-BNBUSDT-daily"
+#indicatorFile <- "ml-LINK-USD-daily-avnnet"
+indicatorFile <- "ml-BATUSDT-daily"
 dailyIndicator <- fread(
   paste(basePath, indicatorFile, ".csv", sep = "")
 )
 dailyIndicator[, Date := as.Date(Date)]
 dailyIndicator <- merge(securityDataTest[, c('Date', 'Eff', 'Actbin')], dailyIndicator, by = "Date")
 
-cat("DAILY EFFECT\n")
 table(
   actualclass = as.character(dailyIndicator$Actbin),
   predictedclass = as.character(dailyIndicator$Action)
