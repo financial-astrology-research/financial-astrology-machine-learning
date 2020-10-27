c4b63e71be4d41f0b238481617b3b3e0eefa52a5
Pablo Cerda <pablo@citla.com>
2020-10-27 11:01:56 +0100

    Refactor ModelLO to include planetY mean orb

--- searchModelXgbLO.R
+++ searchModelXgbLO.R
@@ -15,11 +15,13 @@ source("./indicatorPlots.r")
 
 symbol <- "BAT-USD"
 pxSelect <- c(
-  #'MO',
+  'MO',
   'ME',
   'VE',
   'SU',
-  'MA'
+  'MA',
+  'JU',
+  'SA'
 )
 
 pySelect <- c(
@@ -29,23 +31,23 @@ pySelect <- c(
   'MA',
   'JU',
   'SA',
-  #'NN',
+  'NN',
   'UR',
   'NE',
   'PL'
 )
 
 aspectFilter <- c(
-  #0
-  #30,
-  #45,
-  #60,
-  #90,
-  103
-  #120
-  #135
-  #150
-  #180
+#0
+#30,
+#45,
+#60,
+#90,
+103
+#120
+#135
+#150
+#180
 )
 
 dailyAspectsCount <- dailyAspectsGeneralizedCount(
@@ -69,21 +71,34 @@ dailyAspectsPlanetXCount <- dailyPlanetXActivationCount(
   aspectFilter = aspectFilter
 )
 
+dailyAspectsPlanetYOrb <- dailyPlanetYAspectMeanOrb(
+  orbLimit = 5,
+  pxSelect = pxSelect,
+  pySelect = pySelect,
+  aspectFilter = aspectFilter
+)
+
 dailyAspects <- dailyAspectsCount
 dailyAspects <- merge(dailyAspects, dailyAspectsPlanetYCount, by = c('Date'))
 dailyAspects <- merge(dailyAspects, dailyAspectsPlanetXCount, by = c('Date'))
+dailyAspects <- merge(dailyAspects, dailyAspectsPlanetYOrb, by = c('Date'))
 
 securityData <- mainOpenSecurity(
   symbol, 2, 4,
   "%Y-%m-%d", "2017-01-01", "2020-08-30"
 )
 
-zscoreTrendCut <- 0.5
-securityData[, Actbin := cut(abs(zdiffPercent), c(0, zscoreTrendCut, 10), c('neutral', 'trend'))]
-plot(securityData$Actbin)
+securityData[, zdiffPercent := round(abs(zdiffPercent), 1)]
+securityData[zdiffPercent > 3, zdiffPercent := 3]
+summary(securityData$zdiffPercent)
+hist(securityData$zdiffPercent)
+securityData[,
+  category := cut(zdiffPercent, c(-1, 0.5, 1, 100), c("flat", "low", "high"))
+]
+plot(securityData$category)
 
 aspectView <- merge(
-  securityData[, c('Date', 'Eff', 'Actbin')],
+  securityData[, c('Date', 'category')],
   dailyAspects, by = "Date"
 )
 
@@ -93,141 +108,78 @@ securityDataTest <- mainOpenSecurity(
   "%Y-%m-%d", "2020-09-15"
 )
 
-securityDataTest[, Actbin := cut(abs(zdiffPercent), c(0, zscoreTrendCut, 10), c('neutral', 'trend'))]
-#plot(securityDataTest$Actbin)
+securityDataTest[, zdiffPercent := abs(zdiffPercent)]
+securityDataTest[zdiffPercent > 3, zdiffPercent := 3]
+hist(securityDataTest$zdiffPercent)
+securityDataTest[,
+  category := cut(zdiffPercent, c(-1, 0.5, 1, 100), c("flat", "low", "high"))
+]
+plot(securityDataTest$category)
 
 aspectViewTest <- merge(
-  securityDataTest,
+  securityDataTest[, c('Date', 'category')],
   dailyAspects,
   by = "Date"
 )
 
-trainIndex <- createDataPartition(aspectView$Actbin, p = 0.90, list = FALSE)
+trainIndex <- createDataPartition(aspectView$category, p = 0.90, list = FALSE)
 aspectViewTrain <- aspectView[trainIndex,]
 aspectViewValidate <- aspectView[-trainIndex,]
 
-customSummary <- function (data, lev = levels(data$obs), model = NULL) {
+customSummary <- function(data, lev = levels(data$obs), model = NULL) {
   c(twoClassSummary(data, lev = levels(data$obs), model = NULL))
 }
 
-trainXgbLinearModel <- function() {
-  control <- trainControl(
-    method = "cv",
-    number = 20,
-    savePredictions = "final",
-    returnResamp = "all",
-    classProbs = T,
-    allowParallel = T,
-    verboseIter = T,
-    #summaryFunction = customSummary,
-    trim = F
-  )
-
-  selectCols <- names(aspectViewTrain)[c(-1, -2)]
-  fitModel <- train(
-    formula(Actbin ~ .),
-    data = aspectViewTrain[, ..selectCols],
-    #method = "xgbDART", # 0.51
-    method = "xgbLinear", # 0.52
-    #method = "xgbTree", # 0.45
-    metric = "Kappa",
-    maximize = T,
-    trControl = control,
-    tuneLength = 2
-    #tuneGrid = expand.grid(
-    #  nrounds = 100,
-    #  lambda = 0,
-    #  alpha = 0,
-    #  eta = 0.3
-    #),
-  )
-
-  fitModel$finalModel %>% summary() %>% print()
-  fitModel %>% summary() %>% print()
-  fitModel %>% print()
-  fitModel %>% varImp() %>% print()
-
-  cat("--VALIDATE MODEL--\n\n")
-  # Validate test data accuracy.
-  validateActbinPred <- predict(fitModel, aspectViewValidate, type = "raw")
-  #validateActbinPred <- mapvalues(validateActbinPred, from = c("up", "down"), to = c("buy", "sell"))
-  validateResult <- table(
-    actualclass = as.character(aspectViewValidate$Actbin),
-    predictedclass = as.character(validateActbinPred)
-  ) %>%
-    confusionMatrix(positive = "trend")
-  print(validateResult)
-
-  cat("--TEST MODEL--\n\n")
-  # Validate test data accuracy.
-  testActbinPred <- predict(fitModel, aspectViewTest, type = "raw")
-  #testActbinPred <- mapvalues(testActbinPred, from = c("up", "down"), to = c("buy", "sell"))
-  testResult <- table(
-    actualclass = as.character(aspectViewTest$Actbin),
-    predictedclass = as.character(testActbinPred)
-  ) %>%
-    confusionMatrix(positive = "trend")
-  print(testResult)
-
-  return(fitModel)
-}
-
-fitModel1 <- trainXgbLinearModel()
-fitModel2 <- trainXgbLinearModel()
-fitModel3 <- trainXgbLinearModel()
-
-# Predict outcomes for all weak learner models.
-aspectView$EffNeuP1 <- predict(fitModel1, aspectView, type = "prob")$neutral
-aspectView$EffNeuP2 <- predict(fitModel2, aspectView, type = "prob")$neutral
-aspectView$EffNeuP3 <- predict(fitModel3, aspectView, type = "prob")$neutral
-
-ensambleControl <- trainControl(
-  method = "boot",
-  savePredictions = "final",
+control <- trainControl(
+  method = "cv",
+  number = 10,
+  savePredictions = "all",
   returnResamp = "all",
-  classProbs = T,
   allowParallel = T,
   verboseIter = T,
+  #summaryFunction = customSummary,
   trim = F
 )
 
-# Train ensamble model.
-probCols <- c('EffNeuP1', 'EffNeuP2', 'EffNeuP3')
-ensambleModel <- train(
-  x = aspectView[, ..probCols],
-  y = aspectView$Actbin,
-  method = "gbm",
-  trControl = ensambleControl,
+selectCols <- names(aspectViewTrain)[c(-1)]
+fitModel <- train(
+  formula(category ~ .),
+  data = aspectViewTrain[, ..selectCols],
+  #method = "xgbDART", # 0.51
+  #method = "xgbLinear", # 0.52
+  method = "xgbTree", # 0.45
+  #method = "gamLoess",
+  metric = "Kappa",
+  maximize = T,
+  trControl = control,
   tuneLength = 2
 )
 
-# Predict outcomes for all weak learner models.
-aspectViewTest$EffNeuP1 <- predict(fitModel1, aspectViewTest, type = "prob")$neutral
-aspectViewTest$EffNeuP2 <- predict(fitModel2, aspectViewTest, type = "prob")$neutral
-aspectViewTest$EffNeuP3 <- predict(fitModel3, aspectViewTest, type = "prob")$neutral
+fitModel %>% print()
+fitModel %>% varImp() %>% print()
 
-# Final ensamble prediction.
-aspectViewTest$ActionPred <- predict(ensambleModel, aspectViewTest, type = "raw")
-
-table(
-  actualclass = as.character(aspectViewTest$Actbin),
-  predictedclass = as.character(aspectViewTest$ActionPred)
+cat("--VALIDATE MODEL--\n\n")
+# Validate test data accuracy.
+validateActbinPred <- predict(fitModel, aspectViewValidate, type = "raw")
+#validateActbinPred <- mapvalues(validateActbinPred, from = c("up", "down"), to = c("buy", "sell"))
+validateResult <- table(
+  actualclass = as.character(aspectViewValidate$category),
+  predictedclass = as.character(validateActbinPred)
 ) %>%
-  confusionMatrix(positive = "trend") %>%
-  print()
+  confusionMatrix()
+print(validateResult)
 
-# Full data set prediction.
-dailyAspects$EffNeuP1 <- predict(fitModel1, dailyAspects, type = "prob")$neutral
-dailyAspects$EffNeuP2 <- predict(fitModel2, dailyAspects, type = "prob")$neutral
-dailyAspects$EffNeuP3 <- predict(fitModel3, dailyAspects, type = "prob")$neutral
-dailyAspects$EffPred <- predict(ensambleModel, dailyAspects, type = "raw")
+cat("--TEST MODEL--\n\n")
+# Validate test data accuracy.
+testActbinPred <- predict(fitModel, aspectViewTest, type = "raw")
+#testActbinPred <- mapvalues(testActbinPred, from = c("up", "down"), to = c("buy", "sell"))
+testResult <- table(
+  actualclass = as.character(aspectViewTest$category),
+  predictedclass = as.character(testActbinPred)
+) %>%
+  confusionMatrix()
+print(testResult)
 
-# Round probabilities.
-dailyAspects[, EffNeuP1 := format(EffNeuP1, format = "f", big.mark = ",", digits = 3)]
-dailyAspects[, EffNeuP2 := format(EffNeuP2, format = "f", big.mark = ",", digits = 3)]
-dailyAspects[, EffNeuP3 := format(EffNeuP3, format = "f", big.mark = ",", digits = 3)]
-
-fwrite(dailyAspects, paste("~/Desktop/", symbol, "-predict-xgblinearLO-ensamble", ".csv", sep = ""))
-
+#fwrite(dailyAspects, paste("~/Desktop/", symbol, "-predict-xgblinearLO-ensamble", ".csv", sep = ""))
 #saveRDS(fitModel, paste("./models/", symbol, "_xgb1", ".rds", sep=""))
 #fwrite(dailyAspects, paste("~/Desktop/ml", symbol, "daily-xgb3.csv", sep = "-"))
