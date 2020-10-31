434692679c4cee686402b53dd6d1a9a047d4fd1c
Pablo Cerda <pablo@citla.com>
2020-10-31 21:13:59 +0100

    Model KKNN LDAC, mixed aspect features weak learners ensamble

---
+++ searchModelKknnLDAC.R
@@ -0,0 +1,259 @@
+# Title     : Daily aspects factors GLM logistic model with CV control with aspects factors.
+# Purpose   : Based on ModelLD this model has some variations:
+#             1) Planets MO, ME, VE, SU fast planets applying to all slow planets except JU and NN.
+#             2) Not include absense of planet combination aspect "none".
+#             3) Increase CV folds to 20.
+#             4) Validate fit using Actbin daily price change (buy / sell) instead of Effect
+#                The fit is based on MA(2, 4) effect to smooth price variations.
+#             5) Use kknn in favor of glm as weak learners algorithm.
+#             6) Reduce CV folds to 10.
+#             7) Change data split to 80/20 proportion.
+#             8) Include SU in fast planet.
+#             9) Optimize weak learners and ensamble for Kappa.
+#            10) Train weak learners with different aspects features combinations.
+#            11) Fit weak learners for MA trend and ensamble for Actbin to generalize for daily change.
+
+library(boot)
+library(caret)
+library(psych)
+library(gbm)
+source("./analysis.r")
+source("./indicatorPlots.r")
+
+symbol <- "ADA-USD"
+maPriceFsPeriod <- 2
+maPriceSlPeriod <- 4
+
+pxSelect <- c(
+  'MO',
+  'ME',
+  'VE',
+  'SU',
+  'MA',
+  'JU',
+  'SA'
+)
+
+pySelect <- c(
+  'ME',
+  'VE',
+  'SU',
+  'MA',
+  #'JU',
+  'SA',
+  #'NN',
+  'UR',
+  'NE',
+  'PL'
+)
+
+dailyAspects <- dailyCombPlanetAspectsFactorsTableLI(
+  orbLimit = 2,
+  pxSelect = pxSelect,
+  pySelect = pySelect
+)
+
+control <- trainControl(
+  method = "cv",
+  number = 10,
+  savePredictions = "all",
+  classProbs = T,
+  verboseIter = T,
+  allowParallel = T,
+  trim = F
+)
+
+# Reserved data for validation.
+securityDataTest <- mainOpenSecurity(
+  symbol, maPriceFsPeriod, maPriceSlPeriod,
+  "%Y-%m-%d", "2020-08-01"
+)
+
+aspectViewTest <- merge(
+  securityDataTest,
+  dailyAspects, by = "Date"
+)
+
+modelTrain <- function(method, useFeatures, maPriceFsPeriod, maPriceSlPeriod, modelId) {
+  securityData <- mainOpenSecurity(
+    symbol, maPriceFsPeriod, maPriceSlPeriod,
+    "%Y-%m-%d", "2010-01-01", "2020-06-30"
+  )
+
+  # Filter the extreme outliers.
+  cat(paste("Original days rows: ", nrow(securityData)), "\n")
+  securityData <- securityData[abs(zdiffPercent) < 3]
+  hist(securityData$zdiffPercent)
+  cat(paste("Total days rows: ", nrow(securityData)), "\n")
+
+  aspectView <- merge(
+    securityData[, c('Date', 'diffPercent', 'Actbin', 'Eff')],
+    dailyAspects, by = "Date"
+  )
+
+  selectCols <- c('Eff', useFeatures)
+  cat("Using features: ", selectCols)
+  trainIndex <- createDataPartition(aspectView$Eff, p = 0.80, list = FALSE)
+  aspectViewTrain <- aspectView[trainIndex,]
+  aspectViewValidate <- aspectView[-trainIndex,]
+  fitModel <- train(
+    formula(Eff ~ .),
+    data = aspectViewTrain[, ..selectCols],
+    method = method,
+    metric = "Kappa",
+    trControl = control,
+    tuneLength = 3
+  )
+
+  # Validate data predictions.
+  validateEffPred <- predict(fitModel, aspectViewValidate, type = "raw")
+  aspectViewValidate$EffPred <- mapvalues(validateEffPred, from = c("up", "down"), to = c("buy", "sell"))
+
+  table(
+    actualclass = as.character(aspectViewValidate$Actbin),
+    predictedclass = as.character(aspectViewValidate$EffPred)
+  ) %>%
+    confusionMatrix() %>%
+    print()
+
+  testEffPred <- predict(fitModel, aspectViewTest, type = "raw")
+  aspectViewTest$EffPred <- mapvalues(testEffPred, from = c("up", "down"), to = c("buy", "sell"))
+
+  table(
+    actualclass = as.character(aspectViewTest$Actbin),
+    predictedclass = as.character(aspectViewTest$EffPred)
+  ) %>%
+    confusionMatrix() %>%
+    print()
+
+  #saveRDS(logisticModel, paste("./models/", symbol, "_logistic_", modelId, ".rds", sep = ""))
+  return(fitModel)
+}
+
+allFeatures <- names(dailyAspects)
+useFeatures1 <- allFeatures[grep('^MO|ME|VE', allFeatures)]
+fitModel1 <- modelTrain(
+  "kknn", useFeatures1, 2, 4, "1"
+)
+
+useFeatures2 <- allFeatures[grep('^ME|^VE|^SU', allFeatures)]
+fitModel2 <- modelTrain(
+  "kknn", useFeatures2,2, 4, "2"
+)
+
+useFeatures3 <- allFeatures[grep('^VE|^SU|^MA', allFeatures)]
+fitModel3 <- modelTrain(
+  "kknn", useFeatures3, 2, 4, "3"
+)
+
+useFeatures4 <- allFeatures[grep('^ME|^SU|^MA', allFeatures)]
+fitModel4 <- modelTrain(
+  "kknn", useFeatures4, 2, 4, "4"
+)
+
+useFeatures5 <- allFeatures[grep('^MA|^JU|^SA', allFeatures)]
+fitModel5 <- modelTrain(
+  "kknn", useFeatures5, 3, 6, "5"
+)
+
+fitModel1 %>% print()
+fitModel1 %>% varImp()
+
+fitModel2 %>% print()
+fitModel2 %>% varImp()
+
+fitModel3 %>% print()
+fitModel3 %>% varImp()
+
+fitModel4 %>% print()
+fitModel4 %>% varImp()
+
+fitModel5 %>% print()
+fitModel5 %>% varImp()
+
+securityData <- mainOpenSecurity(
+  symbol, maPriceFsPeriod, maPriceSlPeriod,
+  "%Y-%m-%d", "2010-01-01", "2020-09-30"
+)
+
+# Filter the extreme outliers.
+cat(paste("Original days rows: ", nrow(securityData)), "\n")
+securityData <- securityData[abs(zdiffPercent) < 3]
+hist(securityData$zdiffPercent)
+cat(paste("Total days rows: ", nrow(securityData)), "\n")
+
+aspectView <- merge(
+  securityData[, c('Date', 'diffPercent', 'Actbin', 'Eff')],
+  dailyAspects, by = "Date"
+)
+
+selectCols <- names(aspectView)[c(-1, -2, -3)]
+# Predict outcomes for all weak learner models.
+aspectView$EffUpP1 <- predict(fitModel1, aspectView, type = "prob")$up
+aspectView$EffUpP2 <- predict(fitModel2, aspectView, type = "prob")$up
+aspectView$EffUpP3 <- predict(fitModel3, aspectView, type = "prob")$up
+aspectView$EffUpP4 <- predict(fitModel4, aspectView, type = "prob")$up
+aspectView$EffUpP5 <- predict(fitModel5, aspectView, type = "prob")$up
+
+# Train ensamble model.
+trainIndex <- createDataPartition(aspectView$Actbin, p = 0.80, list = FALSE)
+aspectViewTrain <- aspectView[trainIndex,]
+aspectViewValidate <- aspectView[-trainIndex,]
+
+probCols <- c('EffUpP1', 'EffUpP2', 'EffUpP3', 'EffUpP4', 'EffUpP5')
+topModel <- train(
+  x = aspectViewTrain[, ..probCols],
+  y = aspectViewTrain$Actbin,
+  method = "gbm",
+  metric = "Kappa",
+  trControl = control,
+  tuneLength = 3
+)
+
+topModel %>% summary()
+
+# Validate data predictions.
+aspectViewValidate$EffPred <- predict(topModel, aspectViewValidate, type = "raw")
+
+table(
+  actualclass = as.character(aspectViewValidate$Actbin),
+  predictedclass = as.character(aspectViewValidate$EffPred)
+) %>%
+  confusionMatrix() %>%
+  print()
+
+# Predict outcomes for all weak learner models.
+aspectViewTest$EffUpP1 <- predict(fitModel1, aspectViewTest, type = "prob")$up
+aspectViewTest$EffUpP2 <- predict(fitModel2, aspectViewTest, type = "prob")$up
+aspectViewTest$EffUpP3 <- predict(fitModel3, aspectViewTest, type = "prob")$up
+aspectViewTest$EffUpP4 <- predict(fitModel4, aspectViewTest, type = "prob")$up
+aspectViewTest$EffUpP5 <- predict(fitModel5, aspectViewTest, type = "prob")$up
+# Final ensamble prediction.
+aspectViewTest$EffPred <- predict(topModel, aspectViewTest, type = "raw")
+
+table(
+  actualclass = as.character(aspectViewTest$Actbin),
+  predictedclass = as.character(aspectViewTest$EffPred)
+) %>%
+  confusionMatrix() %>%
+  print()
+
+#saveRDS(topModel, paste("./models/", symbol, "_logistic_ensamble", ".rds", sep = ""))
+
+# Full data set prediction.
+dailyAspects$EffUpP1 <- predict(fitModel1, dailyAspects, type = "prob")$up
+dailyAspects$EffUpP2 <- predict(fitModel2, dailyAspects, type = "prob")$up
+dailyAspects$EffUpP3 <- predict(fitModel3, dailyAspects, type = "prob")$up
+dailyAspects$EffUpP4 <- predict(fitModel4, dailyAspects, type = "prob")$up
+dailyAspects$EffUpP5 <- predict(fitModel5, dailyAspects, type = "prob")$up
+dailyAspects$EffPred <- predict(topModel, dailyAspects, type = "raw")
+
+# Round probabilities.
+dailyAspects[, EffUpP1 := format(EffUpP1, format="f", big.mark = ",", digits = 3)]
+dailyAspects[, EffUpP2 := format(EffUpP2, format="f", big.mark = ",", digits = 3)]
+dailyAspects[, EffUpP3 := format(EffUpP3, format="f", big.mark = ",", digits = 3)]
+dailyAspects[, EffUpP4 := format(EffUpP4, format="f", big.mark = ",", digits = 4)]
+dailyAspects[, EffUpP5 := format(EffUpP5, format="f", big.mark = ",", digits = 5)]
+
+exportCols <- c('Date', selectCols[-1], probCols, "EffPred")
+fwrite(dailyAspects[, ..exportCols], paste("~/Desktop/", symbol, "-predict-kknnLDAC-ensamble", ".csv", sep = ""))
