29b7719be67b0b9ecc62eed2643789d6e65f45bd
Pablo Cerda <pablo@citla.com>
2020-11-01 15:12:06 +0100

    Amend predict filename

--- searchModelKknnLDAF.R
+++ searchModelKknnLDAF.R
@@ -265,4 +265,4 @@ dailyAspects[, EffUpP4 := format(EffUpP4, format="f", big.mark = ",", digits = 4
 dailyAspects[, EffUpP5 := format(EffUpP5, format="f", big.mark = ",", digits = 5)]
 
 exportCols <- c('Date', selectCols[-1], probCols, "EffPred")
-fwrite(dailyAspects[, ..exportCols], paste("~/Desktop/", symbol, "-predict-kknnLDA-ensamble", ".csv", sep = ""))
+fwrite(dailyAspects[, ..exportCols], paste("~/Desktop/", symbol, "-predict-kknnLDAF-ensamble", ".csv", sep = ""))
