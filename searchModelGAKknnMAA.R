ff94658cece91cd83c6ac79573ce648e1afed99f
Pablo Cerda <pablo@citla.com>
2020-11-13 22:45:03 +0100

    Model GA KNNA MAA, remove asteroids and reduce CV repeats

---
+++ searchModelGAKknnMAA.R
@@ -0,0 +1,330 @@
+# Title     : Daily generalized aspects / planet Y activation count KNN regression model
+#             with GA feature selection that maximize Rsquared on train data to fit for
+#             daily price percent change estimation.
+# Purpose   : Based on ModelLD this model has some variations:
+#             1) Planets MO, ME, VE, SU fast planets applying to all slow planets except NN.
+#             2) Weak learners CV folds to 5 with 2 repeats, ensamble folds to 10 with 10 repeats.
+#             3) Split to 80/20 proportion.
+#             4) Validate fit using Actbin daily price change (buy / sell) instead of Effect
+#             5) GA feature detection that fit to maximize Rsquared train data.
+#             6) Fit 5 weak learners for diff percent change.
+#             7) Ensamble weak learnets to fit for Actbin to predict categorical (buy / sell) signal.
+#             8) Optimize weak learners for RMSE.
+#             9) GA feature selection popSize = 100 and iter = 20.
+#            10) KKNN K param set to 7.
+
+library(boot)
+library(caret)
+library(psych)
+library(gbm)
+library(ModelMetrics)
+source("./analysis.r")
+source("./indicatorPlots.r")
+
+symbol <- readline(prompt="Enter a symbol: ")
+zdiffPercentCut <- 2
+maPriceFsPeriod <- 2
+maPriceSlPeriod <- 3
+trainDataStartDate <- as.Date("2010-01-01")
+trainDataEndDate <- as.Date("2020-08-15")
+testDataStartDate <- as.Date("2020-09-01")
+orbLimit <- 4
+kMax <- 7
+gaMaxIter <- 20
+nBits <- 13
+wlCVFolds <- 5
+wlCVRepeats <- 2
+enCVFolds <- 10
+enCVRepeats <- 10
+
+pxSelectAll <- c(
+  'MO',
+  'ME',
+  'VE',
+  'SU'
+)
+
+pySelectAll <- c(
+  'ME',
+  'VE',
+  'SU',
+  'MA',
+  #'CE',
+  #'VS',
+  'JU',
+  'SA',
+  #'NN',
+  #'CH',
+  'UR',
+  'NE',
+  'PL'
+)
+
+idCols <- c('Date', 'Hour')
+setModernMixAspectsSet1()
+setPlanetsMOMEVESUMACEVSJUNNSAURCHNEPL()
+hourlyPlanets <<- openHourlyPlanets('planets_12', clear = F)
+dailyAspectsRows <- dailyHourlyAspectsTablePrepare(hourlyPlanets, idCols, orbLimit)
+
+securityData <- mainOpenSecurity(
+  symbol, maPriceFsPeriod, maPriceSlPeriod,
+  "%Y-%m-%d", trainDataStartDate, trainDataEndDate
+)
+
+# Filter the extreme outliers.
+cat(paste("Original days rows:", nrow(securityData)), "\n")
+securityData <- securityData[abs(zdiffPercent) <= zdiffPercentCut]
+hist(securityData$diffPercent)
+cat(paste("Total days rows:", nrow(securityData)), "\n")
+
+control <- trainControl(
+  method = "repeatedcv",
+  number = wlCVFolds,
+  repeats = wlCVRepeats,
+  savePredictions = "all",
+  verboseIter = F,
+  allowParallel = T,
+  trim = F
+)
+
+prepareDailyAspects <- function(pxSelect, pySelect) {
+  dailyAspectsGeneralizedCount <- dailyAspectsGeneralizedCount(
+    dailyAspects = dailyAspectsRows,
+    orbLimit = orbLimit,
+    pxSelect = pxSelect,
+    pySelect = pySelect
+  )
+
+  dailyPlanetYActivationCount <- dailyPlanetYActivationCount(
+    dailyAspects = dailyAspectsRows,
+    orbLimit = orbLimit,
+    pxSelect = pxSelect,
+    pySelect = pySelect
+  )
+
+  dailyAspects <- merge(dailyAspectsGeneralizedCount, dailyPlanetYActivationCount, date = "Date")
+
+  return(dailyAspects)
+}
+
+modelTrain <- function(pxSelect, pySelect) {
+  if (count(pxSelect) == 0) {
+    pxSelect <- pxSelectAll
+  }
+
+  dailyAspects <- prepareDailyAspects(pxSelect, pySelect)
+  aspectView <- merge(
+    securityData[, c('Date', 'diffPercent', 'Actbin', 'Eff')],
+    dailyAspects, by = "Date"
+  )
+
+  useFeatures <- names(dailyAspects)[-1]
+  selectCols <- c('diffPercent', useFeatures)
+  cat("Using features:", selectCols, "\n")
+
+  trainIndex <- createDataPartition(aspectView$diffPercent, p = 0.80, list = FALSE)
+  aspectViewTrain <- aspectView[trainIndex,]
+  aspectViewValidate <- aspectView[-trainIndex,]
+  fitModel <- train(
+    formula(diffPercent ~ .),
+    data = aspectViewTrain[, ..selectCols],
+    method = "kknn",
+    metric = "RMSE",
+    trControl = control,
+    tuneGrid = expand.grid(
+      kmax = kMax,
+      distance = 2,
+      kernel = "optimal"
+    )
+  )
+
+  # Validate data predictions.
+  validateDiffPercentPred <- predict(fitModel, aspectViewValidate, type = "raw")
+  validatePredictionMSE <- rmse(aspectViewValidate$diffPercent, validateDiffPercentPred)
+  validatePredictionCorrelation <- cor(aspectViewValidate$diffPercent, validateDiffPercentPred)
+  #plot(aspectViewValidate$diffPercent, validateDiffPercentPred)
+  cat("Validate RMSE:", validatePredictionMSE, "\n")
+  cat("Validate Actual/Predicted correlation:", validatePredictionCorrelation, "\n")
+
+  #saveRDS(logisticModel, paste("./models/", symbol, "_logistic_", modelId, ".rds", sep = ""))
+  return(fitModel)
+}
+
+parseSolutionParameters <- function(solution) {
+  pxSelect <- pxSelectAll[solution[1:4] == 1]
+  pySelect <- pySelectAll[solution[5:nBits] == 1]
+
+  return(list(
+    pxSelect = pxSelect,
+    pySelect = pySelect
+  ))
+}
+
+findRelevantFeatures <- function(solution) {
+  params <- parseSolutionParameters(solution)
+  cat("Using PX:", params$pxSelect, "- PY: ", params$pySelect, "\n")
+  fitModel <- modelTrain(params$pxSelect, params$pySelect)
+  cat("Fit Rsquared:", fitModel$results$Rsquared, "\n\n")
+
+  #return(fitModel$results$RMSE)
+  #return(fitModel$results$MAE)
+  return(fitModel$results$Rsquared)
+}
+
+solutionModelTrain <- function(params) {
+  cat("Using PX:", params$pxSelect, "- PY: ", params$pySelect, "\n")
+  fitModel <- modelTrain(params$pxSelect, params$pySelect)
+  cat("Fit Rsquared:", fitModel$results$Rsquared, "\n\n")
+
+  return(fitModel)
+}
+
+gar <- ga(
+  "binary",
+  fitness = findRelevantFeatures,
+  nBits = nBits,
+  names = c(
+    'MOX',
+    'MEX',
+    'VEX',
+    'SUX',
+    'MEY',
+    'VEY',
+    'SUY',
+    'MAY',
+    'CEY',
+    'VSY',
+    'JUY',
+    'SAY',
+    'CHY',
+    'URY',
+    'NEY',
+    'PLY'
+  ),
+  popSize = 100, maxiter = gaMaxIter, run = 20,
+  selection = gabin_rwSelection, mutation = gabin_raMutation,
+  crossover = gabin_spCrossover, population = gabin_Population,
+  parallel = F, monitor = gaMonitor, keepBest = T
+)
+
+summary(gar)
+plot(gar)
+cat("Best GA solution:\n")
+print(gar@solution)
+
+params <- parseSolutionParameters(gar@solution)
+fitModel1 <- solutionModelTrain(params)
+fitModel1 %>% summary()
+fitModel1 %>% varImp()
+
+fitModel2 <- solutionModelTrain(params)
+fitModel2 %>% summary()
+fitModel2 %>% varImp()
+
+fitModel3 <- solutionModelTrain(params)
+fitModel3 %>% summary()
+fitModel3 %>% varImp()
+
+fitModel4 <- solutionModelTrain(params)
+fitModel4 %>% summary()
+fitModel4 %>% varImp()
+
+fitModel5 <- solutionModelTrain(params)
+fitModel5 %>% summary()
+fitModel5 %>% varImp()
+
+dailyAspects <- prepareDailyAspects(params$pxSelect, params$pySelect)
+aspectView <- merge(
+  securityData[, c('Date', 'diffPercent', 'Actbin', 'Eff')],
+  dailyAspects, by = "Date"
+)
+
+# Predict outcomes for all weak learner models.
+aspectView$DiffPred1 <- predict(fitModel1, aspectView, type = "raw")
+aspectView$DiffPred2 <- predict(fitModel2, aspectView, type = "raw")
+aspectView$DiffPred3 <- predict(fitModel3, aspectView, type = "raw")
+aspectView$DiffPred4 <- predict(fitModel4, aspectView, type = "raw")
+aspectView$DiffPred5 <- predict(fitModel5, aspectView, type = "raw")
+
+# Train ensamble model.
+trainIndex <- createDataPartition(aspectView$Actbin, p = 0.80, list = FALSE)
+aspectViewTrain <- aspectView[trainIndex,]
+aspectViewValidate <- aspectView[-trainIndex,]
+
+probCols <- c("DiffPred1", "DiffPred2", "DiffPred3", "DiffPred4", "DiffPred5")
+topModel <- train(
+  x = aspectViewTrain[, ..probCols],
+  y = aspectViewTrain$Actbin,
+  method = "gbm",
+  metric = "Kappa",
+  trControl = trainControl(
+    method = "repeatedcv",
+    number = enCVFolds,
+    repeats = enCVRepeats,
+    savePredictions = "all",
+    classProbs = T,
+    verboseIter = F,
+    allowParallel = T,
+    trim = F
+  ),
+  tuneLength = 3
+)
+
+topModel %>% summary()
+
+# Validate data predictions.
+aspectViewValidate$EffPred <- predict(topModel, aspectViewValidate, type = "raw")
+
+table(
+  actual = aspectViewValidate$Actbin,
+  predicted = aspectViewValidate$EffPred
+) %>% caret::confusionMatrix()
+
+# Reserved data for validation.
+securityDataTest <- mainOpenSecurity(
+  symbol, maPriceFsPeriod, maPriceSlPeriod,
+  "%Y-%m-%d", testDataStartDate
+)
+
+aspectViewTest <- merge(
+  securityDataTest[, c('Date', 'Actbin', 'Eff')],
+  dailyAspects,
+  by = "Date"
+)
+
+# Predict outcomes for all weak learner models.
+aspectViewTest$DiffPred1 <- predict(fitModel1, aspectViewTest, type = "raw")
+aspectViewTest$DiffPred2 <- predict(fitModel2, aspectViewTest, type = "raw")
+aspectViewTest$DiffPred3 <- predict(fitModel3, aspectViewTest, type = "raw")
+aspectViewTest$DiffPred4 <- predict(fitModel4, aspectViewTest, type = "raw")
+aspectViewTest$DiffPred5 <- predict(fitModel5, aspectViewTest, type = "raw")
+# Final ensamble prediction.
+aspectViewTest$EffPred <- predict(topModel, aspectViewTest, type = "raw")
+
+table(
+  actualclass = aspectViewTest$Actbin,
+  predictedclass = aspectViewTest$EffPred
+) %>% caret::confusionMatrix()
+#saveRDS(topModel, paste("./models/", symbol, "_logistic_ensamble", ".rds", sep = ""))
+
+# Full data set prediction.
+dailyAspects$DiffPred1 <- predict(fitModel1, dailyAspects, type = "raw")
+dailyAspects$DiffPred2 <- predict(fitModel2, dailyAspects, type = "raw")
+dailyAspects$DiffPred3 <- predict(fitModel3, dailyAspects, type = "raw")
+dailyAspects$DiffPred4 <- predict(fitModel4, dailyAspects, type = "raw")
+dailyAspects$DiffPred5 <- predict(fitModel5, dailyAspects, type = "raw")
+# Ensamble buy probability and class prediction.
+dailyAspects$BuyProb <- predict(topModel, dailyAspects, type = "prob")$buy
+dailyAspects$EffPred <- predict(topModel, dailyAspects, type = "raw")
+
+# Round probabilities.
+dailyAspects[, DiffPred1 := format(DiffPred1, format = "f", big.mark = ",", digits = 2)]
+dailyAspects[, DiffPred2 := format(DiffPred2, format = "f", big.mark = ",", digits = 2)]
+dailyAspects[, DiffPred3 := format(DiffPred3, format = "f", big.mark = ",", digits = 2)]
+dailyAspects[, DiffPred4 := format(DiffPred4, format = "f", big.mark = ",", digits = 2)]
+dailyAspects[, DiffPred5 := format(DiffPred5, format = "f", big.mark = ",", digits = 2)]
+dailyAspects[, BuyProb := format(BuyProb, format = "f", big.mark = ",", digits = 2)]
+
+aspectsCols <- names(aspectView)[-seq(2, 4)]
+exportCols <- c(aspectsCols, "EffPred")
+fwrite(dailyAspects[, ..exportCols], paste("~/Desktop/", symbol, "-predict-ensamble-gakknn-MAA", ".csv", sep = ""))
