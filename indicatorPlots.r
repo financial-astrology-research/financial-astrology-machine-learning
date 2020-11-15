21133b0da19698a5c9598e308de4e9f0d5d7da9d
Pablo Cerda <pablo@citla.com>
2020-11-15 15:14:03 +0100

    Pre-calculated daily aspects and aspect select at planetX table

--- indicatorPlots.r
+++ indicatorPlots.r
@@ -1763,18 +1763,33 @@ dailyPlanetYActivationCountLDI <- function(orbLimit = 2, pxSelect = c(), pySelec
 }
 
 # Count total aspects per planet X (emitter).
-dailyPlanetXActivationCount <- function(orbLimit = 2, pxSelect = c(), pySelect = c(), aspectFilter = c(), binFlag = F) {
-  idCols <- c('Date', 'Hour')
-  setModernMixAspectsSet1()
-  setPlanetsMOMEVESUMAJUNNSAURNEPL()
-  hourlyPlanets <<- openHourlyPlanets('planets_11', clear = F)
-  dailyAspects <- dailyHourlyAspectsTablePrepare(hourlyPlanets, idCols, orbLimit)
+dailyPlanetXActivationCount <- function(
+  dailyAspects = NULL,
+  orbLimit = 2,
+  pxSelect = c(),
+  pySelect = c(),
+  aspectSelect = c(),
+  aspectFilter = c(),
+  binFlag = F
+) {
+  if (is.null(dailyAspects)) {
+    idCols <- c('Date', 'Hour')
+    setModernMixAspectsSet1()
+    setPlanetsMOMEVESUMAJUNNSAURNEPL()
+    hourlyPlanets <<- openHourlyPlanets('planets_11', clear = F)
+    dailyAspects <- dailyHourlyAspectsTablePrepare(hourlyPlanets, idCols, orbLimit)
+  }
 
   dailyAspects$filter <- F
   dailyAspects[p.x %ni% pxSelect, filter := T]
   dailyAspects[p.y %ni% pySelect, filter := T]
+  dailyAspects[aspect %ni% aspectSelect, filter := T]
   dailyAspects[aspect %in% aspectFilter, filter := T]
   dailyAspects <- dailyAspects[filter != T,]
+
+  if (nrow(dailyAspects) == 0) {
+    return(NULL)
+  }
 
   # Convert numeric aspects to categorical (factors).
   dailyAspects <- dailyAspects[, aspect := as.character(paste("a", aspect, sep = ""))]
@@ -1921,8 +1936,8 @@ dailyFastPlanetsRetrograde <- function() {
     'Date',
     'MESL',
     'VESL'
-        #'SUSL',
-        #'MOSL'
+            #'SUSL',
+            #'MOSL'
   )
 
   return(dailyPlanetsSpeed[, ..selCols])
@@ -1943,8 +1958,8 @@ dailySlowPlanetsRetrograde <- function() {
     'JUSL',
     'SASL',
     'URSL'
-          #'NESL'
-          #'PLSL'
+              #'NESL'
+              #'PLSL'
   )
 
   return(dailyPlanetsSpeed[, ..selCols])
