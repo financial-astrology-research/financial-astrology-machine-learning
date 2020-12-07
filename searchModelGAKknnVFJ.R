8e4beb85a8b2c5b93628ae76ae74515396186262
Pablo Cerda <pablo@citla.com>
2020-12-07 23:51:48 +0100

    Model VF variation: remove MO, add SU/MA/VS/CE/JU/SA/NN/CH/UR/NE to pxSelectgst

--- searchModelGAKknnVFJ.R
+++ searchModelGAKknnVFJ.R
@@ -2,7 +2,7 @@
 #             with GA feature selection that maximize Rsquared on train data to fit for
 #             difflogHxL price change estimation.
 # Purpose   : Predict daily traiding signal action category (buy / sell) from estimated daily price change.
-#             1) Planets MO, ME, VE, SU, MA, CE, VS, JU, SA, NN, CH, UR, NE fast planets applying to all planets, asteroids and NN.
+#             1) Planets ME, VE, SU, MA, CE, VS, JU, SA, NN, CH, UR, NE fast planets applying to all planets, asteroids and NN.
 #             2) CV folds to 5 with 1 repeats for weak learners, folds to 10 with 10 repeats for ensamble.
 #             3) Split to 80/20 proportion.
 #             4) Validate fit using Actbin daily price change (buy / sell) instead of Effect
@@ -25,7 +25,7 @@ library(ModelMetrics)
 source("./analysis.r")
 source("./indicatorPlots.r")
 
-modelId <- "ensamble-gakknn-VFE"
+modelId <- "ensamble-gakknn-VFJ"
 zscoreCut <- 2
 maPriceFsPeriod <- 2
 maPriceSlPeriod <- 3
@@ -42,7 +42,6 @@ enCVFolds <- 10
 enCVRepeats <- 10
 
 pxSelectAll <- c(
-  'MO',
   'ME',
   'VE',
   'SU',
