3b7385486ebe083b859449adfee0883dcac6b9b9
Pablo Cerda <pablo@citla.com>
2020-12-15 11:19:28 +0100

    Compute consensus indicator from symbol/daily index indicators

--- prepareProdPredictions.R
+++ prepareProdPredictions.R
@@ -8,6 +8,17 @@ selectionFile <- "../hisdata/predfiles_selected.csv"
 sourceDirectory <- "~/Desktop/ModelsPred"
 targetDirectory <- "~/Desktop/ModelsProd"
 predictionsList <- fread(selectionFile)
+exportCols <- c('Date', 'EffPred')
+
+getTargetFilePath <- function(symbolId) {
+  targetFilename <- paste("ml", symbolId, "daily.csv", sep = "-")
+  targetFilePath <- paste(targetDirectory, targetFilename, sep = "/")
+}
+
+getConsensusTargetFilePath <- function(symbolId) {
+  targetFilename <- paste("ml", symbolId, "daily-consensus.csv", sep = "-")
+  targetFilePath <- paste(targetDirectory, targetFilename, sep = "/")
+}
 
 preparePredictionCSV <- function(predictFilename) {
   cat("Processing: ", predictFilename, "\n")
@@ -15,10 +26,8 @@ preparePredictionCSV <- function(predictFilename) {
   symbolId <- paste(filenameParts[1], filenameParts[2], "T", sep = "")
   sourceFilePath <- paste(sourceDirectory, predictFilename, sep = "/")
   predictionTable <- fread(sourceFilePath)
-  targetFilename <- paste("ml", symbolId, "daily.csv", sep = "-")
-  targetFilePath <- paste(targetDirectory, targetFilename, sep = "/")
+  targetFilePath <- getTargetFilePath(symbolId)
 
-  exportCols <- c('Date', 'EffPred')
   fwrite(predictionTable[, ..exportCols], targetFilePath)
   cat(symbolId, "predictions exported to:", targetFilePath, "\n")
 
@@ -30,6 +39,11 @@ allSymbolsPredictions <- setDT(rbindlist(lapply(predictionsList$filename, prepar
 allSymbolsPredictions[, Date := as.Date(Date)]
 allSymbolsPredictions[, YearMonth := format(Date, "%Y-%m")]
 allSymbolsPredictions[, YearWeek := format(Date, "%Y-%V")]
+
+getIndexFilePath <- function(indexName) {
+  targetFile <- paste("ml-all", indexName, "index.csv", sep = "-")
+  indexFilePath <- paste(targetDirectory, targetFile, sep = "/")
+}
 
 computeAllSymbolsIndex <- function(byFormula, indexName) {
   allSymbolsIndex <- dcast(
@@ -46,8 +60,7 @@ computeAllSymbolsIndex <- function(byFormula, indexName) {
     Action := ifelse(buy > sell, "buy", ifelse(buy == sell, "neutral", "sell"))
   ]
 
-  targetFile <- paste("ml-all", indexName, "index.csv", sep = "-")
-  indexFilePath <- paste(targetDirectory, targetFile, sep = "/")
+  indexFilePath <- getIndexFilePath(indexName)
   cat("Signals count index exported to:", indexFilePath, "\n")
   fwrite(allSymbolsIndex, indexFilePath)
 }
@@ -55,3 +68,26 @@ computeAllSymbolsIndex <- function(byFormula, indexName) {
 computeAllSymbolsIndex("Date ~ EffPred", "daily")
 computeAllSymbolsIndex("YearWeek ~ EffPred", "weekly")
 computeAllSymbolsIndex("YearMonth ~ EffPred", "monthly")
+
+prepareConsensusPredictionCSV <- function(predictFilename) {
+  cat("Processing: ", predictFilename, "\n")
+  filenameParts <- unlist(strsplit(predictFilename, "-"))
+  symbolId <- paste(filenameParts[1], filenameParts[2], "T", sep = "")
+  targetFilePath <- getTargetFilePath(symbolId)
+  indexFilePath <- getIndexFilePath("daily")
+  symbolIndicator <- fread(targetFilePath)
+  indexIndicator <- fread(indexFilePath)
+  consensusIndicator <- merge(symbolIndicator, indexIndicator[, c('Date', 'Action')], by = "Date")
+  setnames(consensusIndicator, c("Date", "SymbolAction", "IndexAction"))
+
+  # Hold when symbol indicator signal differs from index indicator.
+  consensusIndicator[SymbolAction != IndexAction, EffPred := "hold"]
+  consensusIndicator[SymbolAction == IndexAction, EffPred := SymbolAction]
+  consensusTargetFilePath <- getConsensusTargetFilePath(symbolId)
+  fwrite(consensusIndicator[, ..exportCols], consensusTargetFilePath)
+  cat("Consensus indicator exported to:", consensusTargetFilePath, "\n")
+
+  return(consensusTargetFilePath)
+}
+
+consensusFiles <- lapply(predictionsList$filename, prepareConsensusPredictionCSV)
