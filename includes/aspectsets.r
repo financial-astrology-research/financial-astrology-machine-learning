3058c30cf92c3dbcf96dd99ffff4c2567368a2a9
Pablo Cerda <pablo@citla.com>
2020-12-21 14:20:56 +0100

    Model VAC variation: use major aspects + 135 only

--- includes/aspectsets.r
+++ includes/aspectsets.r
@@ -68,7 +68,6 @@ setClassicAspectsSet7 <- function() {
   defpolarities <<- c(2, 1, 0, 1, 0, 1, 0, 0)
 }
 
-# Limit orbs to 2 degrees which is the max strength.
 setClassicAspectsSet8 <- function() {
   aspects <<- c(0, 30, 45, 60, 90, 120, 135, 150, 180)
   aspectsEnergy <<- c(3, 3, 3, 3, 3, 3, 3, 3, 3)
